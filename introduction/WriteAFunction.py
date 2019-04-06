21cbbe9bf0c93402dd689d7786096a5c0c1e11ff
Rupesh Kumar Saw <38566315+sawrupesh04@users.noreply.github.com>
2019-04-06 23:32:15 +0530

    Update WriteAFunction.py

--- introduction/WriteAFunction.py
+++ introduction/WriteAFunction.py
@@ -2,7 +2,7 @@ def is_leap(year):
     leap = False
 
     if year>=1900 and year<=10**5:
-        if year%4 == 0 and year%100 != 0 or year ==0:
+        if year%4 == 0 and year%100 != 0 or year%400 == 0:
             return True
         return leap
 
